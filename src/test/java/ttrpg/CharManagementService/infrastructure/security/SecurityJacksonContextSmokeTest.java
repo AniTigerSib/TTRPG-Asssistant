ef1babbf0b7ae08fa6bdae2d7fb35860915faca0
Michael <khudoleev04@mail.ru>
2026-05-14 22:25:47 +0500

    refactor: Simplify SecurityJacksonContextSmokeTest configuration and remove unused imports

--- src/test/java/ttrpg/CharManagementService/infrastructure/security/SecurityJacksonContextSmokeTest.java
+++ src/test/java/ttrpg/CharManagementService/infrastructure/security/SecurityJacksonContextSmokeTest.java
@@ -4,16 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jackson.autoconfigure.JacksonAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringJUnitConfig
-@ContextConfiguration(classes = SecurityJacksonContextSmokeTest.TestConfiguration.class)
+@SpringJUnitConfig(SecurityJacksonContextSmokeTest.TestConfiguration.class)
 class SecurityJacksonContextSmokeTest {
 
     @Autowired
@@ -26,11 +25,16 @@ class SecurityJacksonContextSmokeTest {
         assertNotNull(applicationContext.getBean(ApiAccessDeniedHandler.class));
     }
 
+    @Configuration(proxyBeanMethods = false)
     @Import({
-        JacksonAutoConfiguration.class,
         ApiAuthenticationEntryPoint.class,
         ApiAccessDeniedHandler.class
     })
     static class TestConfiguration {
+
+        @Bean
+        ObjectMapper objectMapper() {
+            return new ObjectMapper();
+        }
     }
 }
